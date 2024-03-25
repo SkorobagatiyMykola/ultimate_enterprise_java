696a51178f4fe29f5c55c2b4ac539d16a9f76553
SkorobagatiyMykola <nick1980457@gmail.com>
2024-03-25 16:34:21 +0200

    how to connect TomCat

---
+++ java_networking/src/main/java/ua/skorobahatyi/servlet_lesson13/DemoLess13_1.java
@@ -0,0 +1,12 @@
+package ua.skorobahatyi.servlet_lesson13;
+
+import lombok.SneakyThrows;
+
+public class DemoLess13_1 {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ java_networking/src/main/java/ua/skorobahatyi/servlet_lesson13/MorningServlet.java
@@ -0,0 +1,24 @@
+package ua.skorobahatyi.servlet_lesson13;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet("/morning")
+public class MorningServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var writer = resp.getWriter();
+        var name = Optional.ofNullable(req.getParameter("name"))
+                .orElse("My friend");
+        writer.println("Good morning, " + name);
+
+    }
+}
